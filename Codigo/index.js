add2f06e38e65cd9726f255ead464f901ff8f238
Mohammadus <guilhermevasco08@gmail.com>
2019-06-22 16:23:59 -0300

    Update index.js

--- Codigo/index.js
+++ Codigo/index.js
@@ -11,8 +11,8 @@ var routes = require('./routes');
 // conexão com o banco de dados
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'bolo.admin',
+    password: 'bolo',
     database: 'receitasBD'
 });
 
