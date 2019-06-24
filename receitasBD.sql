a84f53c7b4fbd3a0dcc15bcd1decff29f2cc088b
EnzoItaliano <enzo.ditaliano@gmail.com>
2019-06-24 20:34:03 -0300

    Arquivo sql

--- receitasBD.sql
+++ receitasBD.sql
@@ -10,20 +10,21 @@ GRANT ALL PRIVILEGES ON receitasBD.* TO 'bolo.admin'@'localhost';
 
 use receitasBD;
 
-create table Usuario (
-	idUser int(5) not null AUTO_INCREMENT,
-    senha char(50) not null,
+create table Autor (
+	idAuthor int(5) not null AUTO_INCREMENT,
+    pass char(50) not null,
     nome char(100) not null,
-    email char(50),
+    age int,
+    hability enum('Iniciante', 'Intermediário', 'Avançado'),
     primary key (idUser)
 );
     
-create table Amizade (
-	idUser_1 int(5),
-    idUser_2 int(5),
-    primary key (idUser_1, idUser_2),
-    foreign key (idUser_1) references Usuario(idUser),
-    foreign key (idUser_2) references Usuario(idUser));
+create table Trabalhos (
+	idAuthor int(5),
+    idRestaurant int(5),
+    primary key (idAuthor, idRestaurant),
+    foreign key (idAuthor) references Autor(idAuthor),
+    foreign key (idRestaurant) references Restaurant(idRestaurant));
     
 create table Receita (
 	idReceita int(5) not null AUTO_INCREMENT,
@@ -33,4 +34,9 @@ create table Receita (
     autor int(5),
     primary key (idReceita, autor),
     foreign key (autor) references Usuario(idUser)
+);
+
+create table Restaurant(
+	idRestaurant int(5) not null AUTO_INCREMENT,
+    nome char(100)
 );
