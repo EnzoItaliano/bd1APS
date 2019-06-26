c05df1bbf448e0eabd60e90f6cc074bb05410f7a
Mohammadus <guilhermevasco08@gmail.com>
2019-06-26 19:57:27 -0300

    Create app.js

---
+++ Codigo/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
+
+const port = 8000; //porta padrão
+// var session = require('express-session');
+var routes = require('./routes');
+// var user = require('./routes/user');
+var http = require('http');
+
+// conexão com o banco de dados
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'bolo.admin',
+    password: 'bolo',
+    database: 'receitasBD'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Conectado!');
+});
+global.db = db;
+
+
+const { adicionarAutor, buscarAutor, removerAutor, detalharAutor, editAutor } = require('./routes/author');
+// const { buscarAutor } = require('./routes/author');
+
+
+
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);//call for main index page
+app.post('/cadastroAutor', adicionarAutor);
+// app.get('/autores', buscarAutor);
+app.get('/author/:id', removerAutor);
+
+app.listen(port)
+
+app.get('/author/', buscarAutor);
+
+app.get('/author/editar/:id', detalharAutor);
+app.post('/author/editar', editAutor);
+
+app.get('/cadastroAutor', (req, res) => {
+    res.render('cadastroAutor.ejs')
+})
+
+app.get('/receitas', (req, res) => {
+    res.render('receitas.ejs')
+})
+
+app.get('/index', (req, res) => {
+    res.render('index.ejs')
+})
